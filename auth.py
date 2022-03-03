88865767fc26b6d21ba31db866b75d67a7165089
Débora Peralta <nb28243@MAC130700.local>
2022-03-03 12:44:20 +0100

    modifying auth.py and creating requirements.txt file

--- auth.py
+++ auth.py
@@ -6,14 +6,11 @@ from models import usersFast
 class AuthByToken:
     """ Authorise a connection and making changes with a token given. """
     def auth_by_token(token: str) -> Optional[bool]:
-        users = session.query(usersFast)
-        try:
-            for user in users:
-                if token in user.token:
-                    return True
-        except Exception as e:
-            print(e)
-            return "User not found, try again"
+        users = session.query(usersFast).filter(usersFast.token == token).all()
+        for user in users:
+            if token in user.token:
+                return True
+
 
 if __name__ == '__main__':
     AuthByToken()
